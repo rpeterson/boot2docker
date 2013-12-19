22bcdabdec2a0b7312a86d26d2c7ef2704c6aad5
Matt Apperson <matt@appersonlabs.com>
2013-12-19 13:50:37 -0500

    added auto setup of networking...

--- rootfs/rootfs/bootsync.sh
+++ rootfs/rootfs/bootsync.sh
@@ -18,6 +18,16 @@ fi
 # Configure SSHD
 /etc/rc.d/sshd
 
+ip="$(ifconfig | grep -v 'eth0:' | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+baseip=`echo $ip | cut -d"." -f1-3`
+dgw=$baseip".1"
+
+/sbin/route add -host $dgw eth0
+/sbin/route add default gw $dgw eth0
+
+/bin/echo "nameserver 4.4.4.4" >> /etc/resolv.conf
+/bin/echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
 # Launch Docker
 /etc/rc.d/docker
 
